f6fb7b8b92e6181f043ae46a32c6bcc9a6a3a91c
Dionei Bianchati <dion.biancha@gmail.com>
2020-05-12 00:51:34 -0300

    Updates

---
+++ precache-manifest.0a7b518ffacf344a3880a6bfb5d6db6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0035da75c0013ac1f889eaf4b2596291",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f0bb38a5e41cc441ab0e",
+    "url": "/portfolio/static/css/main.f722aefc.chunk.css"
+  },
+  {
+    "revision": "534a08738e56ce03c204",
+    "url": "/portfolio/static/js/2.31f5ae75.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.31f5ae75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0bb38a5e41cc441ab0e",
+    "url": "/portfolio/static/js/main.8d5561c5.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "f906158415edcd793a08529ca636f53c",
+    "url": "/portfolio/static/media/photo.f9061584.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.3bb3b42cfb3f75cccf8c26465efefe1b.js"
+  "/portfolio/precache-manifest.0a7b518ffacf344a3880a6bfb5d6db6e.js"
 );
 
 self.addEventListener('message', (event) => {
