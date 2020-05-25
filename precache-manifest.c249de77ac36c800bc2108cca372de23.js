b5490b6e65a7c0cf437e2d8a7ecaa3bb0f93d3a2
Dionei Bianchati <dion.biancha@gmail.com>
2020-05-25 19:13:11 -0300

    Updates

---
+++ precache-manifest.c249de77ac36c800bc2108cca372de23.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e73d50d922b5801c546af124fba2822",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "89f8e219e934adf78381",
+    "url": "/portfolio/static/css/2.05a0ae32.chunk.css"
+  },
+  {
+    "revision": "cc821cfc0b2496426212",
+    "url": "/portfolio/static/css/main.6eca88de.chunk.css"
+  },
+  {
+    "revision": "89f8e219e934adf78381",
+    "url": "/portfolio/static/js/2.34ad96b4.chunk.js"
+  },
+  {
+    "revision": "492d7e05a4c2e81da7932422a4d31130",
+    "url": "/portfolio/static/js/2.34ad96b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc821cfc0b2496426212",
+    "url": "/portfolio/static/js/main.48f5cc53.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "2d063bc213441f8c21e1102d8082cef7",
+    "url": "/portfolio/static/media/Diona.2d063bc2.png"
+  },
+  {
+    "revision": "dc387aab57172c0c014edf4aec78d928",
+    "url": "/portfolio/static/media/Laercio.dc387aab.png"
+  },
+  {
+    "revision": "cc28352c2745cb8c07386fcfd24abfd1",
+    "url": "/portfolio/static/media/photo.cc28352c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.621b4b0cee5c8308e88b9a309e67e6d4.js"
+  "/portfolio/precache-manifest.c249de77ac36c800bc2108cca372de23.js"
 );
 
 self.addEventListener('message', (event) => {
