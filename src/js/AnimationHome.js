2176702738fb34070d23f374b35b041d99d0bb39
Dionei Bianchati <dion.biancha@gmail.com>
2020-06-01 10:57:50 -0300

    Home responsive

--- src/js/AnimationHome.js
+++ src/js/AnimationHome.js
@@ -1,8 +1,10 @@
 export function leftOnAnimation() {
   document.getElementById("Text-left").style.marginLeft = '150px';
   document.getElementById("Text-left").style.transitionDuration = '1s';
-  document.getElementById("Left").style.width = '70%';
+  document.getElementById("Left").style.width = '60%';
   document.getElementById("Left").style.transitionDuration = '1s';
+  document.getElementById("Right").style.width = '40%';
+  document.getElementById("Right").style.transitionDuration = '1s';
   document.getElementById("Linkedin").style.transitionDuration = '1s';
   document.getElementById("Linkedin").style.width = '58%';
   document.getElementById("Left").style.zIndex = '3';
@@ -17,6 +19,8 @@ export function leftOffAnimation() {
   document.getElementById("Text-left").style.transitionDuration = '1s';
   document.getElementById("Left").style.width = '50%';
   document.getElementById("Left").style.transitionDuration = '1s';
+  document.getElementById("Right").style.width = '50%';
+  document.getElementById("Right").style.transitionDuration = '1s';
   document.getElementById("Linkedin").style.transitionDuration = '1s';
   document.getElementById("Linkedin").style.width = '50%';
   document.getElementById("Github").style.marginLeft = '0px';
@@ -30,10 +34,12 @@ export function leftOffAnimation() {
 export function rightOnAnimation() {
   document.getElementById("Text-right").style.marginLeft = '80px';
   document.getElementById("Text-right").style.transitionDuration = '1s';
-  document.getElementById("Right").style.width = '70%';
+  document.getElementById("Right").style.width = '60%';
   document.getElementById("Right").style.transitionDuration = '1s';
+  document.getElementById("Left").style.width = '40%';
+  document.getElementById("Left").style.transitionDuration = '1s';
   document.getElementById("Left").style.zIndex = '1';
-  document.getElementById("Right").style.zIndex = '2';
+  document.getElementById("Right").style.zIndex = '3';
   document.getElementById("Github").style.opacity = '0';
   document.getElementById("Github").style.transitionDuration = '1s';
   document.getElementById("Linkedin").style.opacity = '0';
@@ -44,6 +50,8 @@ export function rightOffAnimation() {
   document.getElementById("Text-right").style.transitionDuration = '1s';
   document.getElementById("Right").style.width = '50%';
   document.getElementById("Right").style.transitionDuration = '1s';
+  document.getElementById("Left").style.width = '50%';
+  document.getElementById("Left").style.transitionDuration = '1s';
   document.getElementById("Left").style.zIndex = '1';
   document.getElementById("Right").style.zIndex = '1';
   document.getElementById("Github").style.opacity = '1';
